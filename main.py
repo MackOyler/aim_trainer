5cf1905bf9ed4c2afe42b3c9b279d8d31102f918
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-07-01 11:59:53 -0500

    main loop basics setup

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ WIDTH, HEIGHT = 800, 600
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
+
+def main():
+    run = True
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
