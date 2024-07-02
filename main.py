9e750fcc413e4dc9db97176df4e0059dd9c0b6d1
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-07-02 10:33:35 -0500

    + t/s, speed, hits, lives

--- main.py
+++ main.py
@@ -65,11 +65,20 @@ def format_time(secs):
     
     return f"{minutes:02d}:{seconds:02d}.{milli}"
 
-def draw_top_bar(win, elapse_time, targets_pressed, misses):
+def draw_top_bar(win, elapsed_time, targets_pressed, misses):
     pygame.draw.rect(win, "grey", (0, 0, WIDTH, TOP_BAR_HEIGHT))
-    time_label = LABEL_FONT.render(f"Time: {format_time(elapse_time)}", 1, "black")
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+    
+    speed = round(targets_pressed / elapsed_time, 1)
+    
+    speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "black")
+    hits_label = LABEL_FONT.render(f"Hits: {targets_pressed}", 1, "black")
+    lives_label = LABEL_FONT.render(f"Lives: {LIVES - misses}", 1, "black")
     
     win.blit(time_label, (5, 5))
+    win.blit(speed_label, (200, 5))
+    win.blit(hits_label, (450, 5))
+    win.blit(lives_label, (650, 5))
 
 def main():
     run = True
